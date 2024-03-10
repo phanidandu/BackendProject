02c717d0c430e01a0ebf7df9049fd16fece2e178
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-03-10 22:45:15 +0530

    Using the routers and filters in express

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+   res.status(404).send('<h1>Page not found</h1>')
+})
+
+app.listen(3000);
+/*
+const express= require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product',(req,res,next)=>{
@@ -19,3 +38,4 @@ app.use('/',(req,res,next)=>{
 })
 
 app.listen(3000);
+*/
