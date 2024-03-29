308ee2f5d416f0a4074050d192482426a6c7c601
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-03-29 19:35:18 +0530

    MySQL database connection

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then(result => {
+    console.log(result[0], result[1]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+//app.use(errorController.get404);
+app.render('404', { pageTitle: 'Page Not Found', path: 'Error'});
+
+app.listen(3000);
+
+
+/*
+const path = require('path');
+
 const express= require('express');
 const bodyParser=require('body-parser');
 
@@ -5,18 +44,24 @@ const app=express();
 
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
+const contactRoutes=require('./routes/contactus')
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes)
+//app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-   res.status(404).send('<h1>Page not found</h1>')
-})
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
-/*
+
+
+
 const express= require('express');
 const bodyParser=require('body-parser');
 
@@ -35,6 +80,27 @@ app.use('/product',(req,res,next)=>{
 
 app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express!</h1>');
+})
+
+app.listen(3000);
+*/
+
+/*
+const express= require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+   res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000);
