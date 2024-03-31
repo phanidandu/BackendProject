f9fbadb3dac95d325935e57448b7e00951b2c0a9
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-03-31 20:18:56 +0530

    Using sequelize instead of Mysql

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -14,94 +20,54 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-  .then(result => {
-    console.log(result[0], result[1]);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  console.log(User);
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//app.use(errorController.get404);
-app.render('404', { pageTitle: 'Page Not Found', path: 'Error'});
+app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
-
-/*
-const path = require('path');
-
-const express= require('express');
-const bodyParser=require('body-parser');
-
-const app=express();
-
-const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop')
-const contactRoutes=require('./routes/contactus')
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin',adminRoutes);
-app.use(shopRoutes);
-app.use(contactRoutes)
-//app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
-
-app.listen(3000);
-
-
-
-const express= require('express');
-const bodyParser=require('body-parser');
-
-const app=express();
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>') 
-})
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express!</h1>');
-})
-
-app.listen(3000);
-*/
-
-/*
-const express= require('express');
-const bodyParser=require('body-parser');
-
-const app=express();
-
-const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop')
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
-
-app.use((req,res,next)=>{
-   res.status(404).send('<h1>Page not found</h1>')
-})
-
-app.listen(3000);
-*/
+sequelize
+  //.sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
