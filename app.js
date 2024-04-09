0f595b34aeaf004d5b81c5130c0a5961d35a526b
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-04-09 23:27:48 +0530

    Full stack expense tracker using Express and sequelize

--- app.js
+++ app.js
@@ -3,30 +3,26 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+//const User=require('./models/User');
+var cors=require('cors');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const expenseRoutes=require('./routes/expense');
+
+app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
+app.use(expenseRoutes);
 
 sequelize
   .sync()
   .then(result => {
-    // console.log(result);
-    app.listen(3000);
+    app.listen(3000);      
   })
   .catch(err => {
     console.log(err);
