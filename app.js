21f551bf0094a2ed359e1d86c9cc9680453e9237
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-04-11 16:23:48 +0530

    Adding and deleting products in the cart with many to many relationship

--- app.js
+++ app.js
@@ -3,26 +3,64 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
-//const User=require('./models/User');
-var cors=require('cors');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(cors());
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const expenseRoutes=require('./routes/expense');
-
-app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expenseRoutes);
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync()
+  .sync({ force: true })
+  //.sync()
   .then(result => {
-    app.listen(3000);      
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
