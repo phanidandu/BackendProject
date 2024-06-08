f8951f1b568ac73abf29494eb100d25ec8bea1c5
phanidandu <150283022+phanidandu@users.noreply.github.com>
2024-06-09 00:14:54 +0530

    Integrating payment gateway to the app for premium features
    Integrating payment gateway to the app for premium features

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('./util/database');
 const Expense=require('./models/Expense');
 const User=require('./models/user');
+const Order=require('./models/orders');
 
 const app = express();
 
@@ -13,14 +14,19 @@ app.use(cors());
 
 const userRoutes=require('./routes/user');
 const expenseRoutes=require('./routes/expense');
+const purchaseRoutes=require('./routes/purchase');
 
 app.use(bodyParser.json({ extended: false }));
 
 app.use('/user',userRoutes);
 app.use('/expense',expenseRoutes);
+app.use('/purchase',purchaseRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize
   .sync()
